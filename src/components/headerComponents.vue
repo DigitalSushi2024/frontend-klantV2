3f068e409329d5878afe4b042fb881826fdc2606
Marleen <marleeneijkemans@gmail.com>
2024-11-11 13:11:56 +0100

    remove unnecessary padding- adjust text correct position

--- src/components/headerComponents.vue
+++ src/components/headerComponents.vue
@@ -141,12 +141,7 @@ export default {
   z-index: 3;
   position: relative;
   text-align: center;
-  margin-top: 1em;
-  box-shadow: 0px 1px 1px rgba(3, 7, 18, 0.02),
-  0px 5px 4px rgba(3, 7, 18, 0.03),
-  0px 12px 9px rgba(3, 7, 18, 0.05),
-  0px 20px 15px rgba(3, 7, 18, 0.06),
-  0px 32px 24px rgba(3, 7, 18, 0.08);
+  margin-top: -2.5em;
 }
 
 .cart-modal {
